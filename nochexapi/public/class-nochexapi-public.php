93af444e1b5e4c380c0d51f9a6a9bd7e2dd9fc8d
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2023-06-21 12:08:30 +0100

    Add files via upload

--- nochexapi/public/class-nochexapi-public.php
+++ nochexapi/public/class-nochexapi-public.php
@@ -104,6 +104,75 @@ class Nochexapi_Public {
 
 	}
     
+    public function tp_payment_dupe_check(){
+        global $wpdb;
+        $rootPath                       = NOCHEXAPI::getPluginRootPath() . '/log/cronjobcheck.txt';
+        $successCodes                   = ['000.000.000', '000.100.110'];
+        $logText                        = '';
+        $NochexAPI_Cards               = new NochexAPI_Cards();
+        $isDupeCheckSystemEnabled       = $NochexAPI_Cards->getDupeCheckSystemStatus();
+        $logText                       .= 'isDupeCheckSystemEnabled: ' . $isDupeCheckSystemEnabled . "\n";
+        if( $isDupeCheckSystemEnabled ){
+            $logText                   .= "Dupe System is enabled\n";
+
+            $wpdb->flush();
+            $sqlQuery                   = "SELECT * FROM " . $wpdb->prefix . NOCHEXAPI::GLOBAL_PREFIX . "tnxtbl  
+                                               WHERE status = 0 
+                                                   AND checkout_creation > (NOW() - INTERVAL 30 MINUTE)";
+            $orderTxns                  = $wpdb->get_results( $wpdb->prepare( $sqlQuery ) );
+            $wpdb->flush();
+
+            foreach( $orderTxns AS $singleorderTxn ){
+                $order_id               = $singleorderTxn->post_id;
+                $uuid                   = $singleorderTxn->uuid;
+                $uuid_code              = $singleorderTxn->uuid_code;
+                if( in_array( $uuid_code, $successCodes ) ){
+                    continue;
+                }
+                $logText               .= "Checking orderid:" . $order_id . ", txno:" . $uuid . ", uuid_code:" . $uuid_code . "\n";
+                $checkData              = $NochexAPI_Cards->VerifyPaymentStatusForOrderID( $order_id );
+                $paymentData            = $checkData['payments'][0] ?? [];
+                $checkedResultCode      = (string)$paymentData['result']['code'] ?? '';
+
+                if( in_array( $checkedResultCode, $successCodes ) ){
+                    $order              = wc_get_order( $order_id );
+                    $transaction_id     = (string)$paymentData['id'];
+                    $resultcode         = (string)$paymentData['result']['code'];
+                    $description        = (string)$paymentData['result']['description'];
+                    $paymentType        = (string)$paymentData['paymentType'];
+                    $order->payment_complete( $transaction_id );
+                    wc_reduce_stock_levels($order_id);
+                    $order->add_order_note( 'Your order is paid! Thank you!', true );
+                    $order->add_order_note( $resultcode, false );
+                    $order->add_order_note( $description, false );
+                    $order->add_meta_data( 'paymentType', $paymentType);
+                    $wpdb->update( 
+                        $wpdb->prefix . NOCHEXAPI::GLOBAL_PREFIX . 'tnxtbl',
+                        [ 'status' => 2, 'uuid' => $transaction_id, 'uuid_code' => $resultcode ],
+                        [ 'post_id' => $order_id ]
+                    );
+                    //WC()->cart->empty_cart();
+                    $logText           .= "Successful orderid:" . $order_id . "\n";
+                }
+            }
+        }
+        @file_put_contents( $rootPath, $logText );
+    }
+
+    public function tp_payment_dupe_check_cronstarter_activation(){
+        if( !wp_next_scheduled( NOCHEXAPI::GLOBAL_PREFIX . 'dupe_payment_validation' ) ) {  
+	        wp_schedule_event( time(), 'every10minutes', NOCHEXAPI::GLOBAL_PREFIX . 'dupe_payment_validation' );  
+	    }
+    }
+
+    public function tp_payment_dupe_check_cron_schedules_10min( $schedules ){
+        $schedules['every10minutes'] = array(
+	        'interval' => 10*60,
+	        'display'  => __( 'Once Every 10 Minutes' )
+        );
+        return $schedules;
+    }
+
     public function exclude_from_siteground_script_minification( $exclude_list ){
         $exclude_list[] = NOCHEXAPI::GLOBAL_PREFIX . 'tp_cards';
         return $exclude_list;
